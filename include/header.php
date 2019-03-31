6c175dc2537695c60e5e01958ff374ec0e5e527a
Michelle <wong2580@mylaurier.ca>
2019-03-31 15:12:49 -0400

    Fixed login.php. Added Logout.php. Added PHP to header.php
    Fixed formatting in login.php.
    Added php to login.php to authenticate user.
    
    Added logout (logout.php) as well as butotn in header and made dashboard only visible to users that are logged in.

--- include/header.php
+++ include/header.php
@@ -1,6 +1,9 @@
 <!DOCTYPE html>
 <html lang = "en">
 
+<?php
+session_start(); 
+?>
 <!-- This file contains the header for each page for navigation -->
     <head>
          <!-- Required meta tags -->
@@ -12,9 +15,9 @@
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-        <link href="https://cdn.quilljs.com/1.3.6/quill.snow.css" rel="stylesheet">
+
         <!-- other css -->
-        <link href="css/style2.css" type="text/css" rel="stylesheet">
+        <link href="./css/style2.css" type="text/css" rel="stylesheet">
         <script type="text/javascript" src="js/formCheck.js"></script>
         <title>EverydayFacts</title>
     </head>
@@ -31,18 +34,34 @@
         
             <ul class="nav navbar-nav ml-auto">
                 <li class="nav-item">
-                    <a class="nav-link active" href="#!">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#!">My Facts</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link disabled" href="#!">Post</a>
+                    <a class="nav-link active" href="index.php">All Facts</a>
                 </li>
 
-                <li class="nav-item">
-                    <a class="btn btn-primary" href="#!">Sign Up</a>
-                </li>
+
+                <?php
+                    if ( isset( $_SESSION['userID'])) {
+                        $a = $_SESSION['userID'];
+                ?>
+                    <li class="nav-item">
+                        <a class="nav-link" href="dashboard.php">My Facts</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled" href="#!">Post</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="btn btn-primary" href="./logout.php">Log Out</a>
+                    </li>
+
+                <?php
+                    } else { 
+                ?>
+                    <li class="nav-item">
+                        <a class="btn btn-primary" href="signup.php">Sign Up</a>
+                    </li>
+            
+                <?php
+                }
+                ?>
             </ul>
         </div>
 
--- login.php
+++ login.php
@@ -1,51 +1,52 @@
 <?php
 
 include 'include/header.php';
+include 'functions.php';
 
+$db = getDB();
+$query = "SELECT * FROM Users";
+$result = runQuery($db, $query);
+
+session_start();
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+if (!empty($_POST)){
+    if ( isset( $_POST['username'] ) && isset( $_POST['password'] ) ) {
+        echo "hey";
+        LogIn($username,$password);
+    } 
+}
 ?>
 
 <!DOCTYPE html>
 
 <html>
 <body>
-<div class = "container">
-    <div class="row">
-        <div class="col-xs-5">
-            <h1>Login Page</h1>
-        </div>
-    </div>
-     <br>
-        <form name="login">
+    <div class = "container">
         <div class = "row">
-        <div class="form-group ">
-        <div class="col-xs-2">
-            <input class = "form-control form-control-sm-2" id="user" type="text" name="username" placeholder="Username"><br>
-        </div>
-        </div>
-        </div>
-        <div class = "row">
-        <div class="form-group">
-        <div class="col-xs-2">
-            <input class = "form-control form-control-sm-2" id="pass" type="text" name="password" placeholder="Password">
-        </div>
-        </div>
-        </div>
-        
-        </form>
-        <div class = "row">
-        <div class="col-xs-2">
-        <p><a href="forgetPassword.html">Forgot your Password?</a>
-        </p>
-        </div>
-        </div>
-        <div class = "row">
-        <div class="col-xs-2">
-        <button class="btn btn-secondary" onclick="return validateLogIn()">Login</button>
-        </div>
-        </div>
-        <div class = "row">
-        <div class="col-xs-2">
-    <div class="errobox" id="ero"> </div>
+            <div class = "col">
+                <!-- EMPTY COL: to put login in center --> 
+            </div>
+            <div class = "col-md-4 text-center">
+                <h1> Login Page </h1>
+                    <form  action="" method="post" name="login">
+                        <div class = "form-group mt-4">
+                        <input type="text" class="form-control" name ="username" placeholder="Username">
+                        </div>
+                        <div class = "form-group">
+                        <input type="password" class="form-control" name ="password" placeholder="password">
+                        </div>
+                        <input type="submit" value = "Submit" class="mt-2 btn btn-secondary">
+                    </form>
+                    <div class = "mt-4">
+                        <a href = "forgetPassword.html">Forgot your password?</a>
+                        <div class="errobox" id="ero"> </div>
+                    </div>
+            </div>
+            <div class = "col">
+                <!-- EMPTY COL: to put login in center --> 
+            </div>
     </div>
         </div>
         <!-- Footer -->
@@ -64,7 +65,7 @@ include 'include/header.php';
             </footer>
         
         <!-- Footer -->
-</div>
+        </div>
 </body>
 
 </html>
