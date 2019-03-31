cfdf3d4a12b72d0ae8aee327eae9578815d04776
Michelle <wong2580@mylaurier.ca>
2019-03-30 21:50:30 -0400

    Update everydayfactsDB.sql

--- everydayfactsDB.sql
+++ everydayfactsDB.sql
@@ -9,6 +9,7 @@ CREATE TABLE `Users`(
     `UserID` INTEGER NOT NULL AUTO_INCREMENT,
     `UserName` VARCHAR(50) UNIQUE,
     `Password` VARCHAR(50),
+    `UserDate` DATETIME,
     INDEX (`UserID`),
     PRIMARY KEY(`UserID`)
 ) ENGINE = innodb DEFAULT CHARSET=utf8;
@@ -17,7 +18,19 @@ SET autocommit = 1;
 
 #
 # Dumping data for table `Users`
+#
 
+
+
+CREATE TABLE `Category`(
+    `CategoryID` INTEGER NOT NULL AUTO_INCREMENT,
+    `Type` VARCHAR(50),
+    PRIMARY KEY(`CategoryID`)
+) ENGINE = innodb DEFAULT CHARSET=utf8;
+
+#
+# Dumping data for table 'Category'
+#
 
 CREATE TABLE `Post`(
     `PostID` INTEGER NOT NULL AUTO_INCREMENT,
@@ -25,24 +38,10 @@ CREATE TABLE `Post`(
     `Title` VARCHAR(100),
     `PostDate` DATETIME,
     `CategoryID`INTEGER,
-    INDEX (`PostID, UserID`),
     PRIMARY KEY(`PostID`),
     FOREIGN KEY (`UserID`) REFERENCES Users(`UserID`),
     FOREIGN KEY (`CategoryID`) REFERENCES Category(`CategoryID`)
 ) ENGINE = innodb DEFAULT CHARSET=utf8;
-
-CREATE TABLE `Post`(
-    `PostID` INTEGER NOT NULL AUTO_INCREMENT,
-    `UserID` INTEGER,
-    `Title` VARCHAR(100),
-    `PostDate` DATETIME,
-    `CategoryID`INTEGER,
-    INDEX (`PostID, UserID`),
-    PRIMARY KEY(`PostID`),
-    `UserID` INTEGER FOREIGN KEY REFERENCES Users(`UserID`),
-    FOREIGN KEY (`CategoryID`) REFERENCES Category(`CategoryID`)
-) ENGINE = innodb DEFAULT CHARSET=utf8;
-
 SET autocommit = 1;
 
 CREATE TABLE `Rank`(
@@ -57,13 +56,6 @@ CREATE TABLE `Rank`(
 
 SET autocommit = 1;
 
-CREATE TABLE `Category`(
-    `CategoryID` INTEGER NOT NULL AUTO_INCREMENT,
-    `Type` VARCHAR(50),
-    PRIMARY KEY(`CategoryID`)
-) ENGINE = innodb DEFAULT CHARSET=utf8;
-
-SET autocommit = 1;
 
 CREATE TABLE `Comments`(
     `CommentID` INTEGER NOT NULL AUTO_INCREMENT,
